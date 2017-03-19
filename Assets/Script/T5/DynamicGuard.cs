6765a2e783011cf468b3488503f497e6c00b1769
Kevin Holdcroft <Kevin Holdcroft>
2017-03-19 14:50:54 +0100

    added broken PID control && multistate control

--- Assets/Script/T5/DynamicGuard.cs
+++ Assets/Script/T5/DynamicGuard.cs
@@ -18,6 +18,11 @@ public class DynamicGuard : Point
     private float t_run = 0F;
     private float t_dur = 0F;
     private Vector2 coll_acc;
+    private bool initialRush = true;
+
+    private float[][] integral;
+    private float[][] prev_error;
+
     /*private Vector2 acc = new Vector2();
 
     private void OnDrawGizmos()
@@ -106,10 +111,10 @@ public class DynamicGuard : Point
     }
 
 
-    Vector2 FormationComponent()
+    Vector2 FormationComponent(bool rush)
     {
         float x = 0, y = 0;
-        int j = 0;
+        int j = 0, iteration = 0;
         for (int i = 0; i < this.formation.Count+1; i++) //9 max guards
         {
             if(i == this.guardID)
@@ -121,40 +126,51 @@ public class DynamicGuard : Point
             if (gObj)
             {
                 var pos = gObj.transform.position;
-                x += (pos.x - this.transform.position.x) - this.formation[i-j].x;
-                y += (pos.y - this.transform.position.y) - this.formation[i-j].y;
+                var xerr = (pos.x - this.transform.position.x) - this.formation[i-j].x;
+                var yerr = (pos.y - this.transform.position.y) - this.formation[i-j].y;
+                x += PIDs(xerr, iteration, 0, rush);  //i = 0-4, need 0-6 -> doesn't matter, so long as consistent
+                y += PIDs(yerr, iteration, 1, rush);
 
+
+                if (float.IsNaN(x) || float.IsNaN(y))
+                {
+                    Debug.Log("Fak");
+                }
                 //PID fubar = new PID();
                 //fubar.error;
                 //Debug.Log("x Error between " + this.guardID + " and " + i + " is " + x);
                 //Debug.Log("y Error between " + this.guardID + " and " + i + " is " + y);
             }
+            iteration++;
         }
         this.formationError = new Vector2(x, y);
         return formationError;
     }
 
+    float PIDs(float error, int connection, int xy, bool rush) //0 = x, 1 = y
+    {
+        if (!rush)
+        {
+            if (error < 1)
+                integral[connection][xy] = 0F;
+            integral[connection][xy] += integral[connection][xy] + (error * Time.deltaTime);
+        }
+        var derivative = (error - prev_error[connection][xy]) / Time.deltaTime;
+        var output = Kp * error + Ki * integral[connection][xy] + Kd * derivative;
+        prev_error[connection][xy] = error;
+        return output;
+    }
+
+
     void InitiatePIDs()
     {
-        pid = new PID[this.formation.Count+1];
-        float errx = 0, erry = 0;
-        int j = 0;
-        for (int i = 0; i < this.formation.Count + 1; i++) //9 max guards
+        //int j = 0;
+        integral = new float[formation.Count][];
+        prev_error = new float[formation.Count][];
+        for (int i = 0; i < integral.Length; i++)
         {
-            if (i == this.guardID)
-            {
-                j++;    //hax
-                continue;
-            }
-            var gObj = GameObject.Find("Guard" + i);
-            if (gObj)
-            {
-                var pos = gObj.transform.position;
-                errx += (pos.x - this.transform.position.x) - this.formation[i - j].x;
-                erry += (pos.y - this.transform.position.y) - this.formation[i - j].y;
-
-                pid[i] = new PID();
-            }
+            integral[i] = new float[] { 0, 0 };
+            prev_error[i] = new float[] { 0, 0 };
         }
     }
 
@@ -164,14 +180,14 @@ public class DynamicGuard : Point
         //var correction = pid.Update(setSpeed, actualSpeed, Time.deltaTime);
 
         var goalcomp = GoalComponent();
-        var formcomp = FormationComponent();
+        var formcomp = FormationComponent(false);
         var obsavoid = ObstacleAvoid();
         var edgeavoid = AvoidWalls();
         //Debug.Log("Guard ID: " + guardID + ", Error: " + Mathf.Sqrt(Mathf.Pow(obsavoid.x,2) + Mathf.Pow(obsavoid.y,2)));
         //Debug.Log("Guard: "+guardID+" Edge: (" + edgeavoid.x +", "+edgeavoid.y +")");
+
         var x = goalcomp.x + formcomp.x + obsavoid.x + edgeavoid.x;
         var y = goalcomp.y + formcomp.y + obsavoid.y + edgeavoid.y;
-
 
         var acc = new Vector2(x, y);
         if (acc.magnitude > MAX_ACCEL)
@@ -195,6 +211,64 @@ public class DynamicGuard : Point
         return transform.position + new Vector3(vel.x, vel.y, 0F) * dt;
     }
 
+    Vector3 GetInitialInput()
+    {
+        var formcomp = FormationComponent(true);
+        var obsavoid = ObstacleAvoid();
+        var edgeavoid = AvoidWalls();
+
+        var x = formcomp.x + edgeavoid.x; // + obsavoid.x;// + edgeavoid.x;
+        var y = formcomp.y + edgeavoid.y;// + obsavoid.y;// + edgeavoid.y;
+
+        var acc = new Vector2(x, y);
+        if (acc.magnitude > MAX_ACCEL)
+        {
+            acc.Normalize();
+            acc *= MAX_ACCEL;
+        }
+
+        var dt = Time.deltaTime;
+        vel += acc * dt;
+        if (vel.magnitude > MAX_SPEED)
+        {
+            vel.Normalize();
+            vel *= MAX_SPEED;
+        }
+
+        //we're feeding it position + acceleration componenent, which is wrong
+        Debug.DrawLine(new Vector3(transform.position.x, transform.position.y, 20), new Vector3(transform.position.x + vel.x, transform.position.y + vel.y, 20), Color.green);
+        Debug.DrawLine(new Vector3(transform.position.x, transform.position.y, 20), new Vector3(transform.position.x + acc.x, transform.position.y + acc.y, 20), Color.black);
+        return transform.position + new Vector3(vel.x, vel.y, 0F) * dt;
+    }
+
+    void Formcheck()
+    {
+        float x = 0, y = 0;
+        int j = 0, iteration = 0;
+        for (int i = 0; i < this.formation.Count + 1; i++) //9 max guards
+        {
+            if (i == this.guardID)
+            {
+                j++;    //hax
+                continue;
+            }
+            var gObj = GameObject.Find("Guard" + i);
+            if (gObj)
+            {
+                var pos = gObj.transform.position;
+                x += (pos.x - this.transform.position.x) - this.formation[i - j].x;
+                y += (pos.y - this.transform.position.y) - this.formation[i - j].y;
+
+                if (float.IsNaN(x) || float.IsNaN(y))
+                {
+                    Debug.Log("Fak");
+                }
+            }
+        }
+        var error = new Vector2(x, y).magnitude;
+        if (error < 1) //arbitrary
+            initialRush = false;
+    }
 
     // Use this for initialization
     void Start()
@@ -218,14 +292,22 @@ public class DynamicGuard : Point
         Vector3 input = new Vector3();
         if (inFinalInput == true || Vector3.Distance(transform.position, new Vector3(goalPos[0], goalPos[1], transform.position.z)) < GameManager.endRange)
         {
-            input = getFinalInput();
+            input = getFinalInput();    //Final Input
         }
         else
         {
             input = CollisionImminant();
             if (!collision)
             {
-                input = GetInput();
+                if(initialRush)
+                {
+                    input = GetInitialInput();
+                    Formcheck();   //not inform, in form
+                }
+                else
+                {
+                    input = GetInput();     //Regurlar Input
+                }
             }
             
         }
@@ -264,19 +346,16 @@ public class DynamicGuard : Point
                         if (closestpnt == new Vector3(poly[i][0], poly[i][1], 0F))  //if closest point is the vertices, it is not between the vertices
                         {
                             dirn = new Vector2(this.transform.position.x - poly[i][0], this.transform.position.y - poly[i][1]);
-                            Debug.Log("1");
                         }
                         else if (closestpnt == new Vector3(poly[i][0], poly[i][1], 0F))  //if closest point is the vertices, it is not between the vertices
                         {
-                            dirn = new Vector2(this.transform.position.x - poly[j][0], this.transform.position.y - poly[j][1]);
-                            Debug.Log("2");
+                            dirn = new Vector2(this.transform.position.x - poly[j][0], this.transform.position.y - poly[j][1]); // might need to swap one of these
                         }
                         else
                         {
                             dirn = new Vector2(poly[j][1] - poly[i][1], poly[i][0] - poly[j][0]); //This is backwards. It works and I don't know why
                             dirn.Normalize();
                             dirn *= MAX_ACCEL;
-                            Debug.Log("3");
                         }
                     }
                 }
@@ -297,7 +376,7 @@ public class DynamicGuard : Point
         {
             vel += coll_acc * dt;
             t_run += dt;
-            Debug.DrawLine(new Vector3(transform.position.x, transform.position.y, 20), new Vector3(transform.position.x + vel.x, transform.position.y + vel.y, 20), Color.blue);
+            Debug.DrawLine(new Vector3(transform.position.x, transform.position.y, 20), new Vector3(transform.position.x + vel.x, transform.position.y + vel.y, 20), Color.black);
             Debug.DrawLine(new Vector3(transform.position.x, transform.position.y, 20), new Vector3(transform.position.x + coll_acc.x, transform.position.y + coll_acc.y, 20), Color.red);
 
             return transform.position + new Vector3(vel.x, vel.y, 0F) * dt;
